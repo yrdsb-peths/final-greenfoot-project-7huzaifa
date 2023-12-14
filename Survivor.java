18e2377815a74515865a79e54ebd29c1e71c5b10
huzaifa <350772414@gapps.yrdsb.ca>
2023-12-14 13:32:42 -0500

    updates

--- Survivor.java
+++ Survivor.java
@@ -35,7 +35,6 @@ public class Survivor extends Actor
             shootLeft[i] = new GreenfootImage("images/character/shoot" + i + ".png");
             shootLeft[i].scale(100, 100);
         }
-        
         animationTimer.mark();
         setImage(idleRight[0]);
     }
